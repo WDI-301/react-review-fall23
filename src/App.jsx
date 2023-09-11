49ca36504ef00a9f57bc125f685a3cbd9704d8b5
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-09-11 11:53:23 -0700

    added psuedo code for capitolize function

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { capitolize } from './components/StringFunctions'
+
 
 function App() {
   const [count, setCount] = useState(0)
   const [helloWorld, setHelloWorld] = useState('Hello World')
+  const [inputString, setInputString] = useState('default string')
   
   const setHelloWorldFunc = () => {
     // alert('setHelloWorldInput')
     setHelloWorld('Hello Coders')
   }
+  
 
   return (
     <>
@@ -18,11 +22,16 @@ function App() {
       <div>
         <p>{helloWorld}</p>
       </div>
+      <div className='card'>
+        <h3>inputString: {inputString}</h3>
+      </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button onClick={setHelloWorldFunc}>Set Hello World</button>
+        <button onClick={setHelloWorldFunc}>Set Hello World</button> */}
+      <button onClick={() => capitolize(inputString)}>Capitolize It!</button>
+
       </div>
     </>
   )
---
+++ src/components/StringFunctions.jsx
@@ -0,0 +1,7 @@
+export const capitolize = (inputString) => {
+    // function to capitolize a string of words
+    // get the first character of each word and make it capitol
+    // you can assume each word is separated by a single space - ' '
+
+    // Default String
+}
